c7210eb8f9f33f8e300861102828f386def6ede8
Matheus Ribeiro <omatheusribeiro@outlook.com>
2023-09-21 21:57:48 -0300

    Home module created and future components prepared

---
+++ duplicate-photo-check.Front/src/app/modules/home/home.component.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [HomeComponent]
+})
+export class HomeModule { }
