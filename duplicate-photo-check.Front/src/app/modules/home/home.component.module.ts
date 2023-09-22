33e63cff59a2553f190e5b131b8e7ea8510c179e
Matheus Ribeiro <omatheusribeiro@outlook.com>
2023-09-21 22:44:03 -0300

    Created spinner component

--- duplicate-photo-check.Front/src/app/modules/home/home.component.module.ts
+++ duplicate-photo-check.Front/src/app/modules/home/home.component.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home.component';
 import { ButtonModule } from 'src/app/core/components/button/button.component.module';
 import { InputModule } from 'src/app/core/components/input/input.component.module';
+import { SpinnerModule } from 'src/app/core/components/spinner/spinner.component.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { InputModule } from 'src/app/core/components/input/input.component.modul
   imports: [
     BrowserModule,
     ButtonModule,
-    InputModule
+    InputModule,
+    SpinnerModule
   ],
   providers: [],
   bootstrap: [HomeComponent]
